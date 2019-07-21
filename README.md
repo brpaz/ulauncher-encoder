74b6a54da58e76c19773b2228feed2a10284e4fe
Bruno Paz <brunopaz@sapo.pt>
2019-07-21 11:40:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Ulauncher encoder
+
+[![Ulauncher Extension](https://img.shields.io/badge/Ulauncher-Extension-green.svg?style=for-the-badge)](https://ext.ulauncher.io/-/github-brpaz-ulauncher-encoder)
+[![CircleCI](https://img.shields.io/circleci/build/github/brpaz/ulauncher-encoder.svg?style=for-the-badge)](https://circleci.com/gh/brpaz/ulauncher-encoder)
+![License](https://img.shields.io/github/license/brpaz/ulauncher-encoder.svg?style=for-the-badge)
+
+> [ulauncher](https://ulauncher.io/) extension to Encode/Decode strings into multiple formats like Base64 or URL.
+
+## Requirements
+
+* [ulauncher](https://ulauncher.io/) > 5
+* Python >= 3
+
+## Install
+
+Open ulauncher preferences window -> extensions -> add extension and paste the following url:
+
+```https://github.com/brpaz/ulauncher-netlify```
+
+## Usage
+
+![demo](demo.gif)
+
+This extension supports the following keywords:
+
+* **encode**: To encode a string
+* **decode**: To decode a string
+
+## Development
+
+```
+make link
+make dev
+```
+
+## Contributing
+
+Contributions, issues and Features requests are welcome.
+
+## Show your support
+
+<a href="https://www.buymeacoffee.com/Z1Bu6asGV" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
+
+## License 
+
+Copywright @ 2019 [Bruno Paz](https://github.com/brpaz)
+
+This project is [MIT](LLICENSE) Licensed.
---
+++ main.py
@@ -0,0 +1,99 @@
+"""
+Ulauncher Encoder extension
+"""
+import logging
+import base64
+import urllib
+import html
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+
+LOGGER = logging.getLogger(__name__)
+
+class EncoderExtension(Extension):
+    """ Main Extension class """
+
+    def __init__(self):
+        LOGGER.info('Starting Encoder Extension')
+        super(EncoderExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+    def decode(self, text):
+        """ Decode a string into multiple formats """
+
+        try:
+            decoded_base64 = base64.b64decode(text).decode()
+        except: # pylint: disable=bare-except
+            decoded_base64 = "Cannot decode input text as base64."
+
+        try:
+            decoded_url = urllib.parse.unquote_plus(text)
+        except: # pylint: disable=bare-except
+            decoded_url = "Cannot decode input text"
+
+        decoded_html = html.unescape(text)
+
+        return [
+            ExtensionResultItem(icon='images/icon.png',
+                                name=decoded_base64,
+                                description='Base64 Decoded',
+                                highlightable=False,
+                                on_enter=CopyToClipboardAction(decoded_base64)),
+            ExtensionResultItem(icon='images/icon.png',
+                                name=decoded_url,
+                                description='URL Decoded',
+                                highlightable=False,
+                                on_enter=CopyToClipboardAction(decoded_url)),
+            ExtensionResultItem(icon='images/icon.png',
+                                name=decoded_html,
+                                description='HTML Decoded',
+                                highlightable=False,
+                                on_enter=CopyToClipboardAction(decoded_html))
+        ]
+
+    def encode(self, text):
+        """ Encodes a string into multiple formats """
+
+        encoded_base64 = base64.b64encode(bytes(text, "utf-8")).decode("utf-8")
+        encoded_url = urllib.parse.quote_plus(text)
+        encoded_html = html.escape(text)
+
+        return [
+            ExtensionResultItem(icon='images/icon.png',
+                                name=encoded_base64,
+                                description='Base64 Encoded',
+                                highlightable=False,
+                                on_enter=CopyToClipboardAction(encoded_base64)),
+            ExtensionResultItem(icon='images/icon.png',
+                                name=encoded_url,
+                                description='URL Encoded',
+                                highlightable=False,
+                                on_enter=CopyToClipboardAction(encoded_url)),
+            ExtensionResultItem(icon='images/icon.png',
+                                name=encoded_html,
+                                description='HTML Encoded',
+                                highlightable=False,
+                                on_enter=CopyToClipboardAction(encoded_html))
+        ]
+
+
+class KeywordQueryEventListener(EventListener):
+    """ Handles input events """
+    def on_event(self, event, extension):
+        items = []
+
+        text = event.get_argument() or ""
+        if event.get_keyword() == "encode":
+            items = extension.encode(text)
+        else:
+            items = extension.decode(text)
+
+        return RenderResultListAction(items)
+
+
+if __name__ == '__main__':
+    EncoderExtension().run()
